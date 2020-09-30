8c49db84445a8b4f434a9950ce5f5df8f27ea048
OAVl <yasnastya2010@gmail.com>
2020-10-01 00:04:56 +0300

    portfolio1

--- source/js/jq.js
+++
@@ -1,7 +0,0 @@
-/*$('.page-header__wrapper-header').scroll(function () {
-  $('this').css('background-color', 'red');
-});*/
-$(document).ready(function () {
-  $('page-header__wrapper').css('background-color', 'red');
-});
-
