2bb387095d70baf8629946ddadc9b1b4a5014474
OAVl <yasnastya2010@gmail.com>
2020-10-22 00:39:07 +0300

    правки_map

--- test_techAngar/js/map.js
+++ test_techAngar/js/map.js
@@ -10,56 +10,92 @@ function initMap() {
     disableDefaultUI: true,
     mapTypeControl: true,
     styles : [{
-      featureType : "all",
-      stylers : [{
-        saturation : -100
-      }
+      "featureType": "administrative",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#444444"
+        }
       ]
-    }, {
-      featureType : "all",
-      stylers : [{
-        lightness : -20
+    },
+      {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+          {
+            "color": "#f2f2f2"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+          {
+            "saturation": -100
+          },
+          {
+            "lightness": 45
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+          {
+            "visibility": "simplified"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+          {
+            "color": "#e79d26"
+          },
+          {
+            "visibility": "on"
+          }
+        ]
       }
-      ]
-    }, {
-      featureType : "all",
-      stylers : [{
-        gamma : 0.5
-      }
-      ]
-    }, {
-      featureType : "road",
-      stylers : [{
-        gamma : 0.85
-      }
-      ]
-    }, {
-      featureType : "road.arterial",
-      stylers : [{
-        gamma : 2.6
-      }
-      ]
-    }, {
-      featureType : "road.highway",
-      stylers : [{
-        lightness : 1
-      }
-      ]
-    }, {
-      featureType : "road.highway",
-      stylers : [{
-        gamma : 4
-      }
-      ]
-    }
-    ]
+
+]
   });
 
 
   // Настройки метки
 
   var image = new google.maps.MarkerImage(
-    ' ',
+    '/img/метка.svg',
     new google.maps.Size(130, 130), //размер изображения иконки в пикселях
     new google.maps.Point(0, 0), //позиционирование иконки
     new google.maps.Point(65, 65) //центр иконки
@@ -69,7 +105,7 @@ function initMap() {
     position: myLatLng,
     icon : image,
     map: map,
-    title: 'Фитнес клуб PRIME'
+    title: ''
   });
 
 
