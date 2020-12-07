b1c198a59b1563c77b7ff70cb69034931d8c79e9
OAVl <yasnastya2010@gmail.com>
2020-12-07 22:06:47 +0300

    user

--- test_teplosystem/js/search.js
+++ test_teplosystem/js/search.js
@@ -37,7 +37,7 @@ function autocomplete(inp, arr) {
         /*execute a function when someone clicks on the item value (DIV element):*/
         b.addEventListener("click", function(e) {
           /*insert the value for the autocomplete text field:*/
-          inp.value = this.getElementsByTagName(".page-header__search-input")[0].value;
+          inp.value = this.getElementsByTagName(".search__window")[0].value;
           /*close the list of autocompleted values,
           (or any other open lists of autocompleted values:*/
           closeAllLists();
@@ -45,6 +45,7 @@ function autocomplete(inp, arr) {
         a.appendChild(b);
       }
     }
+
   });
   /*execute a function presses a key on the keyboard:*/
   inp.addEventListener("keydown", function(e) {
